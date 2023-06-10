10c7cd3e7b1eec75c7bb26be382ead8fbedf2a7b
thiagorabaioli <tfrabaioli@outlook.com>
2023-06-10 18:37:39 +0100

    first commit dsmeta app

---
+++ backend/src/main/java/tfrabaioli/rest/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package tfrabaioli.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
