4cdefcb800abd64002ddfe58c1dfd4cc8e2268e0
thiagorabaioli <tfrabaioli@outlook.com>
2023-06-14 11:26:40 +0100

    Date select

--- backend/src/main/java/tfrabaioli/rest/controllers/SaleController.java
+++ backend/src/main/java/tfrabaioli/rest/controllers/SaleController.java
@@ -1,11 +1,16 @@
 package tfrabaioli.rest.controllers;
 
+
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tfrabaioli.rest.entity.Sale;
@@ -24,5 +29,15 @@ public class SaleController {
 		List<Sale> list = service.findSales();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@RequestMapping(value="/pages",method = RequestMethod.GET)
+	public Page<Sale> findSales (
+		@RequestParam(value="minDate", defaultValue = "")String minDate, 
+		@RequestParam(value="maxDate", defaultValue = "")String maxDate, 
+		Pageable pagaeble){
+		return service.findSales(minDate,maxDate,pagaeble);
+		
+	}
+	
 
 }
--- backend/src/main/java/tfrabaioli/rest/services/SaleService.java
+++ backend/src/main/java/tfrabaioli/rest/services/SaleService.java
@@ -1,9 +1,13 @@
 package tfrabaioli.rest.services;
 
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import tfrabaioli.rest.entity.Sale;
@@ -20,6 +24,14 @@ public class SaleService {
 		List<Sale> list = new ArrayList<>();
 		return saleRepo.findAll();
 		
+	}
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
+		
+		LocalDate min = LocalDate.parse(minDate);
+		LocalDate max = LocalDate.parse(maxDate);
+		
+		return saleRepo.findAll(pageable);
 	}
 
 }
